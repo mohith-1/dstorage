281d7fac1deed1c81f343ac97949032fa0153183
mohith-1 <95710094+mohith-1@users.noreply.github.com>
2023-06-14 22:17:15 +0530

    new project

---
+++ src/components/Main.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { convertBytes } from './helpers';
+import moment from 'moment'
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div className="container-fluid mt-5 text-center">
+        <div className="row">
+          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
+            <div className="content">
+              <p>&nbsp;</p>
+              <div className="card mb-3 mx-auto bg-dark" style={{ maxWidth: '512px' }}>
+                <h2 className="text-white text-monospace bg-dark"><b><ins>Share File</ins></b></h2>
+                  <form onSubmit={(event) => {
+                    event.preventDefault()
+                    const description = this.fileDescription.value
+                    this.props.uploadFile(description)
+                  }} >
+                      <div className="form-group">
+                        <br></br>
+                          <input
+                            id="fileDescription"
+                            type="text"
+                            ref={(input) => { this.fileDescription = input }}
+                            className="form-control text-monospace"
+                            placeholder="description..."
+                            required />
+                      </div>
+                    <input type="file" onChange={this.props.captureFile} className="text-white text-monospace"/>
+                    <button type="submit" className="btn-primary btn-block"><b>Upload!</b></button>
+                  </form>
+              </div>
+              
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
